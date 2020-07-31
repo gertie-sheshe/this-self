3327a027cebf8e97064ced9808fc677ad8e89fd7
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-07-31 19:42:54 +0300

    some font size changes

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -3,14 +3,14 @@ import './articles.scss';
 
 const Articles = () => (
     <div className="articles">
-        <h2 className="title">ARTICLES</h2>
+        <h3 className="title">ARTICLES</h3>
         <div className="article-display">
             <a href="medium.com">
                 <div className="container">
                     <i class="fa fa-file-text-o" aria-hidden="true"></i>
                 </div>
                 <div className="article-details">
-                    <h2 className="article-title">Javascript Arrays: Under The Hood + Time Complexity</h2>
+                    <h3 className="article-title">Javascript Arrays: Under The Hood + Time Complexity</h3>
                     <p>Improve performance of Array solutions</p>
                 </div>
             </a>
@@ -21,7 +21,7 @@ const Articles = () => (
                     <i class="fa fa-file-text-o" aria-hidden="true"></i>
                 </div>
                 <div className="article-details">
-                    <h2 className="article-title">Improve Accessibility of Timeline App</h2>
+                    <h3 className="article-title">Improve Accessibility of Timeline App</h3>
                     <p>How I improved the Accessibility of a GitHub Timeline App I built</p>
                 </div>
             </a>
@@ -32,7 +32,7 @@ const Articles = () => (
                     <i class="fa fa-file-text-o" aria-hidden="true"></i>
                 </div>
                 <div className="article-details">
-                    <h2 className="article-title">Web Scraping and UI Testing using Puppeteer</h2>
+                    <h3 className="article-title">Web Scraping and UI Testing using Puppeteer</h3>
                     <p>Working with Puppeteer.</p>
                 </div>
             </a>
@@ -43,7 +43,7 @@ const Articles = () => (
                     <i class="fa fa-file-text-o" aria-hidden="true"></i>
                 </div>
                 <div className="article-details">
-                    <h2 className="article-title">Quadratic to Linear with Javascript Arrays</h2>
+                    <h3 className="article-title">Quadratic to Linear with Javascript Arrays</h3>
                     <p>Working with Puppeteer.</p>
                 </div>
             </a>
--- src/components/talks/talks.js
+++ src/components/talks/talks.js
@@ -3,14 +3,14 @@ import './talks.scss';
 
 const Talks = () => (
     <div className="talks">
-        <h2 className="title">TALKS, WORKSHOPS AND INTERVIEWS</h2>
+        <h3 className="title">TALKS, WORKSHOPS AND INTERVIEWS</h3>
         <div className="talks-display">
             <a href="medium.com">
                 <div className="container">
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                 </div>
                 <div className="talk-details">
-                    <h2 className="talk-title">Taking Accessibility With You</h2>
+                    <h3 className="talk-title">Taking Accessibility With You</h3>
                     <p>Indoni DevConf</p>
                     <p>Cape Town, South Africa - 2020</p>
                 </div>
@@ -22,7 +22,7 @@ const Talks = () => (
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                 </div>
                 <div className="talk-details">
-                    <h2 className="talk-title">Javascript Arrays + Time Complexity</h2>
+                    <h3 className="talk-title">Javascript Arrays + Time Complexity</h3>
                     <p>Facebook Developer Circles: Kigali</p>
                     <p>Kigali, Rwanda - 2020</p>
                 </div>
@@ -34,7 +34,7 @@ const Talks = () => (
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                 </div>
                 <div className="talk-details">
-                    <h2 className="talk-title">Building a Developer Community</h2>
+                    <h3 className="talk-title">Building a Developer Community</h3>
                     <p>Facebook iD8</p>
                     <p>Nairobi, Kenya - 2019</p>
                 </div>
@@ -46,7 +46,7 @@ const Talks = () => (
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                 </div>
                 <div className="talk-details">
-                    <h2 className="talk-title">Introduction to React</h2>
+                    <h3 className="talk-title">Introduction to React</h3>
                     <p>Facebook Developer Circles: Lusaka</p>
                     <p>Kitwe, Zambia - 2018</p>
                 </div>
@@ -58,7 +58,7 @@ const Talks = () => (
                     <i class="fa fa-calendar" aria-hidden="true"></i>
                 </div>
                 <div className="talk-details">
-                    <h2 className="talk-title">Web Accessibility</h2>
+                    <h3 className="talk-title">Web Accessibility</h3>
                     <p>DevCraft Conference</p>
                     <p>Nairobi, Kenya - 2018</p>
                 </div>
