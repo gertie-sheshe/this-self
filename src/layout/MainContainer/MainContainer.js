e9f2c1c67fdea4a934c1654fc38ef7c4927b49a5
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2023-03-19 13:14:45 +0300

    (feat) add Title component with screenreader feature

--- src/layout/MainContainer/MainContainer.js
+++ src/layout/MainContainer/MainContainer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Grid } from "@mui/material";
+import SizeContainer from "gertrudenyenyeshi/layout/SizeContainer";
+
+import { useTheme } from "@mui/material/styles";
 
 function MainContainer({ children }) {
-  return <main>{children}</main>;
+  const theme = useTheme();
+
+  return (
+    <main>
+      <Grid
+        sx={{
+          minHeight: "100vh",
+          width: "100vw",
+          backgroundColor: theme.palette.primary.main,
+        }}
+      >
+        <SizeContainer>{children}</SizeContainer>
+      </Grid>
+    </main>
+  );
 }
 
 export default MainContainer;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,28 +1,12 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import { ScreenReaderTitle } from "./HomeStyles";
 import SummarySection from "gertrudenyenyeshi/layout/SummarySection";
-import SizeContainer from "gertrudenyenyeshi/layout/SizeContainer";
-
-import { useTheme } from "@mui/material/styles";
+import Title from "gertrudenyenyeshi/components/Title";
 
 function Home() {
-  const theme = useTheme();
-
   return (
     <>
-      <ScreenReaderTitle varint="h1">Home</ScreenReaderTitle>
-      <Grid
-        sx={{
-          minHeight: "100vh",
-          width: "100vw",
-          backgroundColor: theme.palette.primary.main,
-        }}
-      >
-        <SizeContainer>
-          <SummarySection />
-        </SizeContainer>
-      </Grid>
+      <Title variant="h1" screenReaderOnly="true" title="Home" />
+      <SummarySection />
     </>
   );
 }
