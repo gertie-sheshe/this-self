692edc69f540a4fca037422d47b8ebb4ca4027a1
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2023-03-19 19:16:18 +0300

    (feat) prop types + data abstraction

--- src/layout/WorkExperienceSection/WorkExperienceSection.js
+++ src/layout/WorkExperienceSection/WorkExperienceSection.js
@@ -1,61 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { List, ListItem, Typography, Grid } from "@mui/material";
 import Title from "gertrudenyenyeshi/components/Title";
 import SectionContainer from "../SectionContainer/SectionContainer";
 import Divider from "gertrudenyenyeshi/components/Divider";
 
-const experienceData = [
-  {
-    title: "Founder",
-    company: "Accessibility Africa",
-    location: "Africa",
-    duration: "2022 - Present",
-    description:
-      "Provider of accessibility services, specializing in audits, training, workshops to help teams build a more accessible web.",
-  },
-  {
-    title: "Senior Frontend Engineer",
-    company: "Civic Tech Company",
-    location: "Cape Town, South Africa",
-    duration: "2020 - 2022",
-    description:
-      "Africa’s largest network of indigenous civic technology and open data laboratories.",
-  },
-  {
-    title: "Software Consultant (Frontend)",
-    company: "Anapanda",
-    duration: "2020",
-    location: "Nairobi",
-    description:
-      "Anapanda is a growth-centric company that helps professionals and organizations reach new levels of alignment, awareness and performance.",
-  },
-  {
-    title: "Web Engineer",
-    company: "BBC",
-    duration: "2018 - 2019",
-    location: "London, UK",
-    description:
-      "The BBC is the world leader in global breaking news, providing trusted, accurate, impartial and independent news ",
-  },
-  {
-    title: "Software Engineer",
-    company: "ShopKeep",
-    duration: "2017",
-    location: "New York",
-    description:
-      "Shopkeep is a cloud-based iPad point of sale featuring inventory management, detailed reporting and 25/7 support.",
-  },
-  {
-    title: "Software Engineer",
-    company: "Urbanstems",
-    location: "Washington D.C",
-    duration: "2016 - 2017",
-    description:
-      "Urbanstems is a flower delivery company based in Washington DC with offices in New York and Austin. ",
-  },
-];
+function WorkExperienceSection({ experienceData }) {
+  if (!experienceData.length) return;
 
-function WorkExperienceSection() {
   return (
     <SectionContainer sx={{ marginTop: "100px" }}>
       <Title
@@ -106,5 +58,17 @@ function WorkExperienceSection() {
     </SectionContainer>
   );
 }
+
+WorkExperienceSection.propTypes = {
+  experienceData: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      company: PropTypes.string,
+      location: PropTypes.string,
+      duration: PropTypes.string,
+      description: PropTypes.string,
+    })
+  ),
+};
 
 export default WorkExperienceSection;
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { summarySectionProps } from "gertrudenyenyeshi/config";
+import {
+  summarySectionProps,
+  workExperienceSectionProps,
+} from "gertrudenyenyeshi/config";
 import SummarySection from "gertrudenyenyeshi/layout/SummarySection";
 import WorkExperienceSection from "gertrudenyenyeshi/layout/WorkExperienceSection";
 import Title from "gertrudenyenyeshi/components/Title";
@@ -9,7 +12,7 @@ function Home() {
     <>
       <Title variant="h1" screenReaderOnly="true" title="Home" />
       <SummarySection {...summarySectionProps} />
-      <WorkExperienceSection />
+      <WorkExperienceSection {...workExperienceSectionProps} />
     </>
   );
 }
